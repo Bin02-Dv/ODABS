c4ca673fca988945468b4214a7f5d4b860d54e04
Bin02-Dv <alaminbinahmad@gmail.com>
2025-07-31 16:05:53 -0700

    first commit

--- ODABSApp/views.py
+++ ODABSApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -13,3 +15,41 @@ def d_dash(request):
 
 def a_dash(request):
     return render(request, "admin/a-dash.html")
+
+def patient_signUp(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if User.objects.filter(email=email).exists():
+            return JsonResponse(
+                {
+                    "message": "Sorry Email already exist!!",
+                    "status": False
+                }
+            )
+        
+        elif confirm_password != password:
+            return JsonResponse(
+                {
+                    "message": "Sorry your password and confirm password is missed match!!",
+                    "status": False
+                }
+            )
+        
+        else:
+            new_user = User.objects.create_user(first_name=full_name, email=email, password=password)
+            return JsonResponse(
+                {
+                    "message": "Patient Registered successfully...",
+                    "status": True
+                }
+            )
+    return JsonResponse(
+        {
+            "message": "No request was sent!!",
+            "status": False
+        }
+    )
