ec6ae53326ee3cbfe3973a3ca0988831ab7fac1f
Bin02-Dv <alaminbinahmad@gmail.com>
2025-08-01 18:25:40 -0700

    8/1/2025 - 6:25pm

--- ODABSApp/urls.py
+++ ODABSApp/urls.py
@@ -3,12 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
     
     # patient dashboard
     path('dash/', views.dash, name="dash"),
+    path('patient-signUp/', views.patient_signUp, name="patient-signUp"),
     
     # doctor dashboard
     path('d-dash/', views.d_dash, name="d-dash"),
+    path('doctor-signUp/', views.doctor_signUp, name="doctor-signUp"),
     
     # admin dashboard
     path('a-dash/', views.a_dash, name="a-dash")
--- ODABSApp/views.py
+++ ODABSApp/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
 
+@login_required(login_url='/')
 def dash(request):
     return render(request, "patient/p-dash.html")
 
+@login_required(login_url='/')
 def d_dash(request):
     return render(request, "doctor/d-dash.html")
 
+@login_required(login_url='/')
 def a_dash(request):
     return render(request, "admin/a-dash.html")
 
@@ -20,6 +25,7 @@ def patient_signUp(request):
     if request.method == 'POST':
         full_name = request.POST['full_name']
         email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
         
@@ -27,7 +33,15 @@ def patient_signUp(request):
             return JsonResponse(
                 {
                     "message": "Sorry Email already exist!!",
-                    "status": False
+                    "success": False
+                }
+            )
+        
+        elif User.objects.filter(username=username).exists():
+            return JsonResponse(
+                {
+                    "message": "Sorry Username already exist!!",
+                    "success": False
                 }
             )
         
@@ -35,21 +49,111 @@ def patient_signUp(request):
             return JsonResponse(
                 {
                     "message": "Sorry your password and confirm password is missed match!!",
-                    "status": False
+                    "success": False
                 }
             )
         
         else:
-            new_user = User.objects.create_user(first_name=full_name, email=email, password=password)
+            new_user = User.objects.create_user(first_name=full_name, email=email, last_name='patient', username=username, password=password)
             return JsonResponse(
                 {
-                    "message": "Patient Registered successfully...",
-                    "status": True
+                    "message": "Your account has been Registered successfully...",
+                    "success": True
                 }
             )
     return JsonResponse(
         {
             "message": "No request was sent!!",
-            "status": False
+            "success": False
         }
     )
+
+
+def doctor_signUp(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if User.objects.filter(email=email).exists():
+            return JsonResponse(
+                {
+                    "message": "Sorry Email already exist!!",
+                    "success": False
+                }
+            )
+        
+        elif User.objects.filter(username=username).exists():
+            return JsonResponse(
+                {
+                    "message": "Sorry Username already exist!!",
+                    "success": False
+                }
+            )
+        
+        elif confirm_password != password:
+            return JsonResponse(
+                {
+                    "message": "Sorry your password and confirm password is missed match!!",
+                    "success": False
+                }
+            )
+        
+        else:
+            new_user = User.objects.create_user(first_name=full_name, email=email, last_name='doctor', username=username, password=password)
+            return JsonResponse(
+                {
+                    "message": "Your account has been Registered successfully...",
+                    "success": True
+                }
+            )
+    return JsonResponse(
+        {
+            "message": "No request was sent!!",
+            "success": False
+        }
+    )
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        url = ''
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            if user.is_superuser:
+                url = '/a-dash/'
+            elif user.last_name == 'doctor':
+                url = '/d-dash/'
+            else:
+                url = '/dash/'
+            auth.login(request, user)
+            return JsonResponse(
+                {
+                    "message": "Login successfully...",
+                    "success": True,
+                    "url": url
+                }
+            )
+        else:
+            return JsonResponse(
+                {
+                    "message": "Invalid Username or password",
+                    "success": False
+                }
+            )
+    return JsonResponse(
+        {
+            "message": "No request sent!!",
+            "success": False
+        }
+    )
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
